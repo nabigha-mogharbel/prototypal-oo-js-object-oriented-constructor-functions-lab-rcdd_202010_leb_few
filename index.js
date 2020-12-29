2866dda22b9a36cd971aaee7e017b227c8d3c114
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-12-29 00:56:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function Driver(name, age, experience){
   this.age=age
   this.experience=experience
 }
-function PickupLocation(adress, city){
-  this.adress=adress
+function PickupLocation(address, city){
+  this.address=adress
   this.city=city
 }
